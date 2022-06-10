564a49d56aec5519501b9ebf7c278ce5ecc25bed
DavidH1982 <david.houghton1982@gmail.com>
2022-06-10 22:34:18 +0100

    Updates

---
+++ precache-manifest.541fd978e188cee70a3a8b98629f9805.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "aaa7c56dea9d7328e588",
+    "url": "/1minus1-clone/static/css/main.60007810.chunk.css"
+  },
+  {
+    "revision": "aaa7c56dea9d7328e588",
+    "url": "/1minus1-clone/static/js/main.aaa7c56d.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/1minus1-clone/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/1minus1-clone/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "800530674a07dbb5be41",
+    "url": "/1minus1-clone/static/js/runtime~main.80053067.js"
+  },
+  {
+    "revision": "21844da48daf03b99934164fa3cacc5a",
+    "url": "/1minus1-clone/static/media/1minus1_jumbo_dancer_Moment.21844da4.jpg"
+  },
+  {
+    "revision": "8384d867ce03b42ea33b7fb00971af22",
+    "url": "/1minus1-clone/static/media/xboxstudios.8384d867.png"
+  },
+  {
+    "revision": "2435453ea760f60f68a111d1b16b7309",
+    "url": "/1minus1-clone/static/media/turn10logo_1.2435453e.png"
+  },
+  {
+    "revision": "3891b23f957fca8b6ade2ce34d169d8c",
+    "url": "/1minus1-clone/static/media/thatgamelogo.3891b23f.png"
+  },
+  {
+    "revision": "eb2928a4c6316eb8357fa295b3f017f8",
+    "url": "/1minus1-clone/static/media/studiotypeslogo.eb2928a4.png"
+  },
+  {
+    "revision": "c01133dad7dae987dd4cfc512c766c34",
+    "url": "/1minus1-clone/static/media/lightspeedlogo.c01133da.png"
+  },
+  {
+    "revision": "649c8e4385b1ce30a79d4097a9eaed81",
+    "url": "/1minus1-clone/static/media/tezotopialogo.649c8e43.png"
+  },
+  {
+    "revision": "0841a54e1d4967a98b396392473e5fa2",
+    "url": "/1minus1-clone/static/media/turn10carimage.0841a54e.png"
+  },
+  {
+    "revision": "25881ed1738819d74d99c67217a46913",
+    "url": "/1minus1-clone/static/media/ballisticastronautimage.25881ed1.png"
+  },
+  {
+    "revision": "bbf78f44992093211b712a15788dca1e",
+    "url": "/1minus1-clone/static/media/fasttravelzombieimage.bbf78f44.png"
+  },
+  {
+    "revision": "a14aa6d44c4aaa4048ed79b27831c187",
+    "url": "/1minus1-clone/static/media/playground_games_phone_Moment.a14aa6d4.jpg"
+  },
+  {
+    "revision": "42ab5127190890f58ef89f9b5cad5844",
+    "url": "/1minus1-clone/static/media/FontsFree-Net-Gilroy3.42ab5127.ttf"
+  },
+  {
+    "revision": "fb2bd48650a5accbdbd0476573ad81a9",
+    "url": "/1minus1-clone/static/media/vrlogo.fb2bd486.webp"
+  },
+  {
+    "revision": "8d38c83c02fa48f57107fdd6fa84ae20",
+    "url": "/1minus1-clone/static/media/fasttravellogo.8d38c83c.png"
+  },
+  {
+    "revision": "40c0c2fb0bdec71cf21706a366142406",
+    "url": "/1minus1-clone/static/media/handsController.40c0c2fb.png"
+  },
+  {
+    "revision": "ffc3fc0ef401f34c9ff40a7e17d95cf8",
+    "url": "/1minus1-clone/static/media/featuredprojectvideo_Moment.ffc3fc0e.jpg"
+  },
+  {
+    "revision": "7a76a65ef7ce76e6eb0e83f12c218c17",
+    "url": "/1minus1-clone/static/media/studiotypeswireframeimage.7a76a65e.png"
+  },
+  {
+    "revision": "040673a92f47d38f7909b727bb57e3d6",
+    "url": "/1minus1-clone/index.html"
+  }
+];
